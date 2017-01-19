3d1ce20e36ae3cb94296b9ed04f4db27c0e88ff0
Serafettin Yilmaz <serafettinyilmaz04@gmail.com>
2017-01-19 14:15:11 +0200

    Complete bag module

---
+++ bag.py
@@ -0,0 +1,15 @@
+class Bag():
+
+	def __init__(self):
+		self.bag = []
+		self.fill_bag()
+
+	def fill_bag(self):
+		self.bag.extend(['Q', 'Z', 'J', 'X', 'K'])
+		for i in range(2): self.bag.extend(['F', 'H', 'V', 'W', 'Y', 'B', 'C', 'M', 'P', '@'])
+		for i in range(3): self.bag.extend(['G'])
+		for i in range(4): self.bag.extend(['D', 'U', 'S', 'L'])
+		for i in range(6): self.bag.extend(['T', 'R', 'N'])
+		for i in range(8): self.bag.extend(['O'])
+		for i in range(9): self.bag.extend(['I', 'A'])
+		for i in range(12): self.bag.extend(['E'])
