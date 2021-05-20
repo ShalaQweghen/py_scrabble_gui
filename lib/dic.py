c71c38d27e6e5f8459e6e1d03dd3bdf80a4f5fdc
Serafettin Yilmaz <serafettinyilmaz04@gmail.com>
2021-05-20 03:35:53 +0300

    Improve dictionary lookup

--- lib/dic.py
+++ lib/dic.py
@@ -2,9 +2,16 @@
 #
 # See 'py_scrabble.pyw' for more info on copyright
 
+from collections import defaultdict
+
 class Dict:
   def __init__(self, dic):
-    self.dict = open(dic).read().splitlines()
+    words = open(dic).read().splitlines()
+
+    self.dict = defaultdict(lambda: False)
+    
+    for word in words:
+      self.dict[word] = True
 
   def valid_word(self, word):
-    return word in self.dict
+    return self.dict[word]
